def09336c93035da6fcb1e06dcf4a9f65c33b842
Arin Jain <arinjain7d@gmail.com>
2025-11-19 16:02:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6791680409-aba3d",
+  "appId": "1:647535845144:web:03adcabc6ed1606eaf886d",
+  "apiKey": "AIzaSyCvncL1bhbVpW5lPNa7G4bXu2ox2g08nOQ",
+  "authDomain": "studio-6791680409-aba3d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "647535845144"
+};
